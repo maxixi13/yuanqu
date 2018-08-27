8092a291d7e31340f5491d6652f90e40d358d1aa
maxixi <524092363@qq.com>
2018-08-27 14:18:48 +0800

    个人页面布局 icon

--- app/src/main/java/com/example/maxixi/yuanqu/fragment/Fragmentpersonal.java
+++ app/src/main/java/com/example/maxixi/yuanqu/fragment/Fragmentpersonal.java
@@ -2,20 +2,25 @@ package com.example.maxixi.yuanqu.fragment;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
+import android.widget.Toolbar;
 
 import com.example.maxixi.yuanqu.R;
+
 
 public class Fragmentpersonal extends Fragment{
 
     @Override
     public View onCreateView(LayoutInflater inflater,ViewGroup container,Bundle saveInstanceState){
         View view=inflater.inflate(R.layout.dctivity_personal,container,false);
+
+        //toolbar_menu
+        Toolbar toolbar=(Toolbar)view.findViewById(R.id.personal_toolbar);
+        toolbar.inflateMenu(R.menu.personal_toolbar);
+
         return view;
     }
 }
