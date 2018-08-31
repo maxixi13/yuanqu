318a165781ea919c9b6b1ad8da832fd9e2e6137d
maxixi <524092363@qq.com>
2018-08-31 18:17:56 +0800

    recyclerview嵌套recyclerview

---
+++ app/src/main/java/com/example/maxixi/yuanqu/cloud/cloud_adapter/cloud_zhidao_adapter.java
@@ -0,0 +1,123 @@
+package com.example.maxixi.yuanqu.cloud.cloud_adapter;
+
+
+
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.maxixi.yuanqu.R;
+
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static android.media.CamcorderProfile.get;
+
+
+public class cloud_zhidao_adapter extends RecyclerView.Adapter<cloud_zhidao_adapter.ViewHolder>{
+
+
+    //点击事件
+//    private OnItemClickListener mOnItemClickListener;
+//
+//    public void setOnItemClickListener(OnItemClickListener mOnItemClickListener){
+//        this.mOnItemClickListener = mOnItemClickListener;
+//    }
+//
+//    public interface OnItemClickListener{
+//        void onItemClick(View view,int position);
+//    }
+//
+
+
+    private List<cloud_zhidao_lei> myzhidaolist;
+    private List<cloud_zhidao_leichild> zhidaochildList=new ArrayList<>();
+
+        static class ViewHolder extends RecyclerView.ViewHolder{
+            TextView zhidaoText;
+            public ViewHolder(View view){
+                super(view);
+                zhidaoText=(TextView)view.findViewById(R.id.chuangye_zhidao_name);
+
+            }
+
+        }
+
+    public cloud_zhidao_adapter(List<cloud_zhidao_lei>zhidaolist){ myzhidaolist=zhidaolist; }
+
+        @Override
+        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType){
+            View view= LayoutInflater.from(parent.getContext()).inflate(R.layout.cctivity_cloud_chuangye_zhidao_item,parent,false);
+            ViewHolder holder=new ViewHolder(view);
+
+            //viewlist嵌套
+            initzhidaochildList();
+            RecyclerView recyclerView=(RecyclerView)view.findViewById(R.id.cloud_chuangye_recyclerchild);
+            LinearLayoutManager layoutManager=new LinearLayoutManager(parent.getContext());
+            recyclerView.setLayoutManager(layoutManager);
+            cloud_zhidao_adapterchild cloud_zhidao_adapterchild=new cloud_zhidao_adapterchild(zhidaochildList);
+            recyclerView.setAdapter(cloud_zhidao_adapterchild);
+            recyclerView.setNestedScrollingEnabled(false);
+
+
+
+            return holder;
+        }
+
+    @Override
+    public void onBindViewHolder(final ViewHolder holder, final int position){
+        final cloud_zhidao_lei cloud_zhidao_lei = myzhidaolist.get(position);
+        holder.zhidaoText.setText(cloud_zhidao_lei.getname());
+
+
+
+
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(holder.itemView.getContext(),"巴巴爸爸"+get(position)+position,Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+//        //判断是否设置了监听器 点击事件
+//        if(mOnItemClickListener != null){
+//            //为ItemView设置监听器
+//            holder.itemView.setOnClickListener(new View.OnClickListener() {
+//                @Override
+//                public void onClick(View v) {
+//                    int position = holder.getLayoutPosition(); // 1
+//                    mOnItemClickListener.onItemClick(holder.itemView,position); // 2
+//                }
+//            });
+//        }
+
+
+
+        }
+
+    @Override
+    public int getItemCount(){
+        return myzhidaolist.size();
+    }
+
+
+
+    private void initzhidaochildList(){
+            cloud_zhidao_leichild madada=new cloud_zhidao_leichild("hahahah","2018-07-08");
+            zhidaochildList.add(madada);
+            cloud_zhidao_leichild madada1=new cloud_zhidao_leichild("hahahah","2018-00-00");
+            zhidaochildList.add(madada1);
+            cloud_zhidao_leichild madada11=new cloud_zhidao_leichild("hahahah","2015-00-00");
+            zhidaochildList.add(madada11);
+
+    }
+
+}
---
+++ app/src/main/java/com/example/maxixi/yuanqu/cloud/cloud_adapter/cloud_zhidao_lei.java
@@ -0,0 +1,9 @@
+package com.example.maxixi.yuanqu.cloud.cloud_adapter;
+
+public class cloud_zhidao_lei {
+    private String name;
+
+    public cloud_zhidao_lei(String name){ this.name=name; }
+    public String getname() { return name; }
+
+}
