f2226c5f3ef16fb0e9898e1c2148ab74b35f6cd8
maxixi <524092363@qq.com>
2018-09-17 13:09:47 +0800

    2018-09-17 点餐页面卡顿

--- app/src/main/java/com/example/maxixi/yuanqu/SplashActivity.java
+++ app/src/main/java/com/example/maxixi/yuanqu/SplashActivity.java
@@ -1,12 +1,15 @@
 package com.example.maxixi.yuanqu;
 
 import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.content.Intent;
+import android.nfc.Tag;
 import android.os.CountDownTimer;
 import android.os.Handler;
 import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
@@ -18,6 +21,7 @@ import java.util.TimerTask;
 
 public class SplashActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,37 +30,38 @@ public class SplashActivity extends AppCompatActivity {
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);//隐藏状态栏
 
 
-       final TextView  textView = (TextView) findViewById(R.id.tv);
-//        Message message = handler.obtainMessage(1);
-//        handler.sendMessageDelayed(message, 1000);
-//
-        Button button=(Button)findViewById(R.id.tvb);
+        final TextView textView = (TextView) findViewById(R.id.tv);
+
+
+        Button button = (Button) findViewById(R.id.tvb);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent mainIntent = new Intent(SplashActivity.this, MainActivity.class);
+                Intent mainIntent = new Intent(SplashActivity.this, Loginpage.class);
                 startActivity(mainIntent);
                 SplashActivity.this.finish();
             }
         });
 
+
         CountDownTimer countDownTimer = new CountDownTimer(5000, 1000) {
             @Override
             public void onTick(long millisUntilFinished) {
-                textView.setText(millisUntilFinished/1000 + "s");
+                textView.setText(millisUntilFinished / 1000 + "s");
+                if (SplashActivity.this.isFinishing()) {
+                    cancel();
+                }
             }
 
             @Override
             public void onFinish() {
 
-                Intent mainIntent = new Intent(SplashActivity.this, MainActivity.class);
+                Intent mainIntent = new Intent(SplashActivity.this, Loginpage.class);
                 startActivity(mainIntent);
                 SplashActivity.this.finish();
 
             }
         }.start();
-
-
     }
 
 //    private Handler handler = new Handler() {
--- app/src/main/java/com/example/maxixi/yuanqu/fragment/Glogindenglufg.java
+++ app/src/main/java/com/example/maxixi/yuanqu/fragment/Glogindenglufg.java
@@ -1,20 +1,73 @@
 package com.example.maxixi.yuanqu.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+import com.example.maxixi.yuanqu.MainActivity;
 import com.example.maxixi.yuanqu.R;
 
-public class Glogindenglufg extends Fragment{
+import static com.example.maxixi.yuanqu.R.color.white;
+
+public class Glogindenglufg extends Fragment {
+
+    private CountDownTimer countDownTimer;
 
 
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle saveInstanceState) {
-            View view = inflater.inflate(R.layout.activity_loginpage_denglu, container, false);
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle saveInstanceState) {
+        View view = inflater.inflate(R.layout.activity_loginpage_denglu, container, false);
 
-            return view;
+        Button denglu = (Button) view.findViewById(R.id.login_denglubutton);
+        denglu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getContext(), MainActivity.class);
+                startActivity(intent);
+                getActivity().finish();
+            }
+        });
+
+        final Button yanzhengma = (Button) view.findViewById(R.id.login_yanzhengmabutton);
+        yanzhengma.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                jishiqi(yanzhengma);
+            }
+        });
+
+
+        return view;
+    }
+
+
+    private void jishiqi(final Button button) {
+
+        if(countDownTimer!=null) {
+            countDownTimer.cancel(); //防止new出多个导致时间跳动加速
+            countDownTimer=null;
         }
+        countDownTimer = new CountDownTimer(60 * 1000, 1000) {
+
+            @Override
+            public void onTick(long millisUntilFinished) {
+                button.setText(millisUntilFinished / 1000 + "s");
+                if(getActivity()==null){
+                    cancel();
+                }
+            }
+
+            @Override
+            public void onFinish() {
+                button.setText("获取验证码");
+
+            }
+        }.start();
+    }
+
 }
