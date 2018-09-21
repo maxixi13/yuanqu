9e6531721304c00c1d578442fa4d4a1852b31f04
maxixi <524092363@qq.com>
2018-09-21 15:56:03 +0800

    2018-09-17 传递数据 diancan-queren //点餐页面卡顿

--- app/src/main/java/com/example/maxixi/yuanqu/personal/renzhengqiye.java
+++ app/src/main/java/com/example/maxixi/yuanqu/personal/renzhengqiye.java
@@ -1,11 +1,14 @@
 package com.example.maxixi.yuanqu.personal;
 
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.Toolbar;
 
 import com.example.maxixi.yuanqu.R;
+
 
 public class renzhengqiye extends AppCompatActivity {
 
@@ -21,5 +24,20 @@ public class renzhengqiye extends AppCompatActivity {
                 finish();
             }
         });
+
+        ImageView upimg = (ImageView) findViewById(R.id.personal_renzheng_upimgview);
+        upimg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Dialog();
+
+            }
+        });
     }
 }
+
+
+
+//https://www.jianshu.com/p/26d29e187f89
+
+//https://www.jianshu.com/p/26d29e187f89
